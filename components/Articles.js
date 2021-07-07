bef8197107cb70d56801d176e03c6450c32e44c4
RauboLuk <rauboluk@gmail.com>
2021-07-07 22:28:03 +0200

    lesson-1

---
+++ components/Articles.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Articles.module.css";
+
+const Articles = () => {
+  return (
+    <div>
+      <h2 className={styles.header}>Articles</h2>
+    </div>
+  );
+};
+
+export default Articles;
