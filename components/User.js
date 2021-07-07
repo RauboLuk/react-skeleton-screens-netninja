59ee06660f2ba9438bda6bccb34a6e93ccd59dd5
RauboLuk <rauboluk@gmail.com>
2021-07-07 22:28:03 +0200

    lesson-1

---
+++ components/User.js
@@ -0,0 +1,11 @@
+import styles from "../styles/User.module.css";
+
+const User = () => {
+  return (
+    <div>
+      <h2 className={styles.header}>User Details</h2>
+    </div>
+  );
+};
+
+export default User;
